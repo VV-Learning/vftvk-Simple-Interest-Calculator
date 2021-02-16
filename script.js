0cbbbffa82e5ed6a8ec6f13c884c7173a86bffd1
vikraver <35995493+vikraver@users.noreply.github.com>
2021-02-16 22:26:55 +0530

    Fully functional
    Fully Functional code

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 function compute()
 {
+    //Read inputs and validate principal. If principal is zero or negative flash an error.
     var principal = document.getElementById("principal").value;
-    //Check if principal field is zero ir negative
-        if(principal <= 0){
+            if(principal <= 0){
             alert("Please enter a positive number");
             principal.focus();
             return false;
           }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
+
+    //Calculate interest and cumulative amount
     var interest = principal*years*rate/100;
     var Amount = Number(principal) + Number(interest);
+
+    //Determine the year when the investor will get the Amount
     var date = new Date();
     var currentyear = date.getFullYear();
     var lastyear = Number(currentyear) + Number(years);
-         
+    
+    //Pass result to 'result' element on web page
    document.getElementById("result").innerHTML="If you deposit <mark>" + principal + "</mark>, <br>at an interest rate of <mark>" + rate + "</mark>%, <br>You will receive an interest of <mark>" + interest + "</mark>,<br>i.e. a total amount of <mark>" + Amount + "</mark> in the year <mark>" + lastyear + "</mark>.";
 }
 
 function show_value(x)
 {
+    //Function to show slider value next to Interest Rate
     document.getElementById("rate_value").innerHTML=x;
 }
         
