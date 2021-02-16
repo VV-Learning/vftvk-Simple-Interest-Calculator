f963db52835e13aee0e87ca0711bfd0536e36f84
vikraver <35995493+vikraver@users.noreply.github.com>
2021-02-16 20:56:51 +0530

    Version 1

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal*years*rate/100;
+    var Amount = Number(principal) + Number(interest);
+    var date = new Date();
+    var currentyear = date.getFullYear();
+    var lastyear = Number(currentyear) + Number(years);
+     
     
+   document.getElementById("result").innerHTML="If you deposit " + principal + ", at an interest rate of " + rate + "%, You will receive an interest of " + interest + ", i.e. a total amount of " + Amount + " in the year " + lastyear;
+}
+
+function show_value(x)
+{
+    document.getElementById("rate_value").innerHTML=x;
 }
         
